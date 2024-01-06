83a5fda67678e8e962ebba5e3d6da2fe75873a86
yeon <test2@test2.com>
2024-01-06 17:12:49 +0900

    오류 고쳐보기

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // API
 // import config from '../src/config.js';
 const apiKey = config.apiKey;
-const accessToken = config.accessToken;
+// const accessToken = config.accessToken;
 
 const options = {
 	method: 'GET',
 	headers: {
 		accept: 'application/json',
-		Authorization: `Bearer ${accessToken}`,
+		Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDI2MmNhZTU2NTg4OTIyZmJiYmZmYjQ3NzIyOTI2ZiIsInN1YiI6IjY1OGUyOWY5ZDc1YmQ2NDE0ZTcyOTczMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R7gFfyBqP0L6j3F1MaKwImsNmvCin8Apwoh00iKAlOo`,
 	},
 };
 

f0b83b6ba3e5941b0412d1487c58aa49ea9e92c0
yeon <test2@test2.com>
2024-01-06 17:16:52 +0900

    config 오류 지워보기

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 const config = {
 	apiKey: '24262cae56588922fbbbffb47722926f',
-	// accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDI2MmNhZTU2NTg4OTIyZmJiYmZmYjQ3NzIyOTI2ZiIsInN1YiI6IjY1OGUyOWY5ZDc1YmQ2NDE0ZTcyOTczMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R7gFfyBqP0L6j3F1MaKwImsNmvCin8Apwoh00iKAlOo',
 };
